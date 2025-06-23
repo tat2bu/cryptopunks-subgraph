4f5d1d5c0756881d1898a5158328c10fea32e162
Simon Oualid <simon.oualid@gmail.com>
2025-06-23 22:08:50 +0200

    w

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -26,6 +26,10 @@ export let washTrades = [
   '0xa4fa9c0976e550a27184c1ffd17f55f770c9afa429b7cd8495004302a82722c5'
 ];
 
+// export const ONLY_ON_TX:string = "0xe0f91066dec4acedd280a2d1d6d8a3e81e8e66664036bba2dc933301968f72fb";
+export const ONLY_ON_TX:string = "";
+// export const ONLY_ON_TX:string = "0xf4bf0d474fb8fcaa069799e754ad9a6fbd4b7a46eb2d02963f51468521af7121";
+
 export const TARGET_TOKENS = [
   Bytes.fromHexString("0xb7f7f6c52f2e2fdb1963eab30438024864c313f6")!,
   Bytes.fromHexString("0x000000000000003607fce1ac9e043a86675c5c2f")!,
